8f763b45e78d9df388640df397e7a908cb54e2ff
Maxim <Maxim1296@yandex.ru>
2018-07-10 18:52:40 +0300

    Clear admin web project

--- LMS.Admin.Web/Startup.cs
+++ LMS.Admin.Web/Startup.cs
@@ -18,7 +18,6 @@ namespace LMS.Admin.Web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
@@ -30,7 +29,6 @@ namespace LMS.Admin.Web
             return new AutofacServiceProvider(container);
         }
         
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
