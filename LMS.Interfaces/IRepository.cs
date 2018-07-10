b6728cb691e09208fa1a32b21c119eecaadbc79d
Maxim <Maxim1296@yandex.ru>
2018-07-10 12:28:34 +0300

    Update IRepository

--- LMS.Interfaces/IRepository.cs
+++ LMS.Interfaces/IRepository.cs
@@ -6,8 +6,9 @@ namespace LMS.Interfaces
     public interface IRepository<T> where T : class
     {
         IEnumerable<T> GetAll();
+        IEnumerable<T> Filter(Func<T, Boolean> predicate);
         T Get(int id);
-        IEnumerable<T> Find(Func<T, Boolean> predicate);
+        T Find(Func<T, Boolean> predicate);
         void Create(T item);
         void Update(T item);
         void Delete(int id);
