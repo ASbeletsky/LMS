653771062b6a6a752948a3ba780d111cfa3f140c
Maxim <Maxim1296@yandex.ru>
2018-07-11 16:07:47 +0300

    Add repositories to IUnitOfWork

--- LMS.Interfaces/IUnitOfWork.cs
+++ LMS.Interfaces/IUnitOfWork.cs
@@ -1,9 +1,16 @@
 ﻿using System.Threading.Tasks;
+using LMS.Entities;
 
 namespace LMS.Interfaces
 {
     public interface IUnitOfWork
     {
+        IRepository<Choice> Choices { get; }
+        IRepository<ProblemType> ProblemTypes { get; }
+        IRepository<TestProblem> Problems { get; }
+        IRepository<TestCategory> TestCategories { get; }
+        IRepository<Test> Tests { get; }
+
         Task SaveAsync();
     }
 }
