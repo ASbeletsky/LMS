804fa1708e3e9a701b59145e4d39a49e6a9623c8
Maxim <Maxim1296@yandex.ru>
2018-07-13 18:58:49 +0300

    Add validation attributes to QuestionDTO

--- LMS.Dto/QuestionDTO.cs
+++ LMS.Dto/QuestionDTO.cs
@@ -1,16 +1,28 @@
-﻿namespace LMS.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LMS.Dto
 {
     public class QuestionDTO
     {
         public int Id { get; set; }
+
+        [Range(0, 10, ErrorMessage = "Value out of range")]
         public int Complexity { get; set; }
+
+        [Required(ErrorMessage = "Content must be defined")]
         public string Content { get; set; }
-        
+
+        [Display(Name = "Is visible")]
         public bool IsVisible { get; set; }
 
         public QuestionTypeDTO Type { get; set; }
+
+        [Display(Name = "Type")]
         public int TypeId { get; set; }
+
         public CategoryDTO Category { get; set; }
+
+        [Display(Name = "Category")]
         public int CategoryId { get; set; }
     }
 }
