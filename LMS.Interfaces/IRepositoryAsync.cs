7110bd2f516732839c9c474007c115c8ec6bcc2b
Михайло Саввін <savvin@dlit.dp.ua>
2018-07-16 10:39:05 +0300

    Add IRepositoryAsync

---
+++ LMS.Interfaces/IRepositoryAsync.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LMS.Interfaces
+{
+    public interface IRepositoryAsync<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<IEnumerable<T>> Filter(Func<T, bool> predicate);
+        Task<T> GetAsync(int id);
+        Task<T> FindAsync(Func<T, bool> predicate);
+        Task CreateAsync(T item);
+        Task UpdateAsync(T item);
+        Task DeleteAsync(int id);
+    }
+}
