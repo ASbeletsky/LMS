c23dbe4a19dc12d3dfca3f39bbd038d236ca5cbb
Megaremzik <megaremzik@gmail.com>
2018-07-19 08:57:23 +0300

    js functions moved to a separate file

---
+++ LMS.Client.Web/wwwroot/js/Functions.js
@@ -0,0 +1,6 @@
+﻿function changeMode(mode) {
+    document.querySelector('input[name=mode]').value = mode;
+}
+function changeGot(got) {
+    document.querySelector('input[name=got]').value = got;
+}
