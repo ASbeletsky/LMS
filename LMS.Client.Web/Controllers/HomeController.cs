3b87386f98f80b1f93acca32acb68596dbbdda78
Megaremzik <megaremzik@gmail.com>
2018-07-19 20:13:12 +0300

    Added new model
    Added model TestInfo, for additional info for views. And changed views so that they worked with this model.

--- LMS.Client.Web/Controllers/HomeController.cs
+++ LMS.Client.Web/Controllers/HomeController.cs
@@ -10,6 +10,83 @@ namespace LMS.Client.Web.Controllers
 {
     public class HomeController : Controller
     {
+        TestInfo info = new TestInfo
+        {
+            test = new Test
+            {
+                Id = 1,
+                Title = "Тест для принятия на работу",
+                Category = new TestCategory { Id = 1, Title = ".Net" },
+                Duration = new System.TimeSpan(1, 0, 0),
+                Problems = new List<TestProblem>
+                {
+                    new TestProblem
+                    {
+                        Id = 1,
+                        Complexity = 2,
+                        Content = "Будет true or false?",
+                        Type = new ProblemType { Id = 0, Title = "Тест на выбор одного правильного ответа" },
+                        Test = new Test(),
+                        Choices = new List<Choice> { new Choice { Id = 1, Answer = "True", IsRight = true, Problem = new TestProblem() }, new Choice { Id = 2, Answer = "False", IsRight = false, Problem = new TestProblem() } }
+                    },
+                    new TestProblem
+                    {
+                        Id = 3,
+                        Complexity = 2,
+                        Content = "Доброе утро?",
+                        Type = new ProblemType { Id = 0, Title = "Тест на выбор одного правильного ответа" },
+                        Test = new Test(),
+                        Choices = new List<Choice> { new Choice { Id = 1, Answer = "Да", IsRight = true, Problem = new TestProblem() }, new Choice { Id = 2, Answer = "Нет", IsRight = false, Problem = new TestProblem() }, new Choice { Id = 3, Answer = "ХЗ", IsRight = true, Problem = new TestProblem() } }
+                    },
+                    new TestProblem
+                    {
+                        Id = 2,
+                        Complexity = 2,
+                        Content = "Как дела?",
+                        Type = new ProblemType { Id = 1, Title = "Тест на выбор нескольких ответов" },
+                        Test = new Test(),
+                        Choices = new List<Choice> { new Choice { Id = 1, Answer = "Хорошо", IsRight = true, Problem = new TestProblem() }, new Choice { Id = 2, Answer = "Плохо", IsRight = false, Problem = new TestProblem() }, new Choice { Id = 3, Answer = "Норм", IsRight = true, Problem = new TestProblem() } }
+                    },
+                    new TestProblem
+                    {
+                        Id = 4,
+                        Complexity = 2,
+                        Content = "Франция чемпион?",
+                        Type = new ProblemType { Id = 0, Title = "Тест на выбор одного правильного ответа" },
+                        Test = new Test(),
+                        Choices = new List<Choice> { new Choice { Id = 1, Answer = "Да", IsRight = true, Problem = new TestProblem() }, new Choice { Id = 2, Answer = "Нет", IsRight = false, Problem = new TestProblem() }, new Choice { Id = 3, Answer = "ХЗ", IsRight = true, Problem = new TestProblem() } }
+                    },
+                    new TestProblem
+                    {
+                        Id = 5,
+                        Complexity = 2,
+                        Content = "Объясните что такое полиморфизм",
+                        Type = new ProblemType { Id = 2, Title = "Тест на написание развёрнутого ответа" },
+                        Test = new Test(),
+                        Choices = new List<Choice>()
+                    },
+                    new TestProblem
+                    {
+                        Id = 6,
+                        Complexity = 2,
+                        Content = "Напишите Hello World",
+                        Type = new ProblemType { Id = 3, Title = "Тест на написание кода" },
+                        Test = new Test(),
+                        Choices = new List<Choice>()
+                    },
+                    new TestProblem
+                    {
+                        Id = 7,
+                        Complexity = 2,
+                        Content = "Покажите схему заказа в Маке",
+                        Type = new ProblemType { Id = 4, Title = "Тест на диаграмму" },
+                        Test = new Test(),
+                        Choices = new List<Choice>()
+                    }
+                }
+            },
+            CurrentQuestionNumber = 0
+        };
         public IActionResult Index()
         {
             return View();
@@ -34,98 +111,23 @@ namespace LMS.Client.Web.Controllers
         }
         public IActionResult Greetings()
         {
-            Test helptest = new Test
-            {
-                Id = 1,
-                Title = "Тест для принятия на работу",
-                Category = new TestCategory { Id = 1, Title = ".Net" },
-                Duration = new System.TimeSpan(1, 0, 0),
-                Problems = new List<TestProblem>(20)
-            };
-            return View(helptest);
+            return View(info);
         }
         public ActionResult ShowProblem(int number)
         {
-            List<TestProblem> helpProblem = new List<TestProblem>
-            {
-                new TestProblem
-                {
-                    Id = 1,
-                    Complexity = 2,
-                    Content = "Будет true or false?",
-                    Type = new ProblemType { Id = 0, Title = "Тест на выбор одного правильного ответа" },
-                    Test = new Test(),
-                    Choices = new List<Choice> { new Choice { Id = 1, Answer = "True", IsRight = true, Problem = new TestProblem() }, new Choice { Id = 2, Answer = "False", IsRight = false, Problem = new TestProblem() } }
-                },
-                new TestProblem
-                {
-                    Id = 3,
-                    Complexity = 2,
-                    Content = "Доброе утро?",
-                    Type = new ProblemType { Id = 0, Title = "Тест на выбор одного правильного ответа" },
-                    Test = new Test(),
-                    Choices = new List<Choice> { new Choice { Id = 1, Answer = "Да", IsRight = true, Problem = new TestProblem() }, new Choice { Id = 2, Answer = "Нет", IsRight = false, Problem = new TestProblem() }, new Choice { Id = 3, Answer = "ХЗ", IsRight = true, Problem = new TestProblem() } }
-                },
-                new TestProblem
-                {
-                    Id = 2,
-                    Complexity = 2,
-                    Content = "Как дела?",
-                    Type = new ProblemType { Id = 1, Title = "Тест на выбор нескольких ответов" },
-                    Test = new Test(),
-                    Choices = new List<Choice> { new Choice { Id = 1, Answer = "Хорошо", IsRight = true, Problem = new TestProblem() }, new Choice { Id = 2, Answer = "Плохо", IsRight = false, Problem = new TestProblem() }, new Choice { Id = 3, Answer = "Норм", IsRight = true, Problem = new TestProblem() } }
-                },
-                new TestProblem
-                {
-                    Id = 4,
-                    Complexity = 2,
-                    Content = "Франция чемпион?",
-                    Type = new ProblemType { Id = 0, Title = "Тест на выбор одного правильного ответа" },
-                    Test = new Test(),
-                    Choices = new List<Choice> { new Choice { Id = 1, Answer = "Да", IsRight = true, Problem = new TestProblem() }, new Choice { Id = 2, Answer = "Нет", IsRight = false, Problem = new TestProblem() }, new Choice { Id = 3, Answer = "ХЗ", IsRight = true, Problem = new TestProblem() } }
-                },
-                new TestProblem
-                {
-                    Id = 5,
-                    Complexity = 2,
-                    Content = "Объясните что такое полиморфизм",
-                    Type = new ProblemType { Id = 2, Title = "Тест на написание развёрнутого ответа" },
-                    Test = new Test(),
-                    Choices = new List<Choice>()
-                },
-                new TestProblem
-                {
-                    Id = 6,
-                    Complexity = 2,
-                    Content = "Напишите Hello World",
-                    Type = new ProblemType { Id = 3, Title = "Тест на написание кода" },
-                    Test = new Test(),
-                    Choices = new List<Choice>()
-                },
-                new TestProblem
-                {
-                    Id = 7,
-                    Complexity = 2,
-                    Content = "Покажите схему заказа в Маке",
-                    Type = new ProblemType { Id = 4, Title = "Тест на диаграмму" },
-                    Test = new Test(),
-                    Choices = new List<Choice>()
-                }
-            };
-            ViewBag.Index = number;
-            ViewBag.Kolvo = helpProblem.Count;
-            switch (helpProblem[number].Type.Id)
+            info.CurrentQuestionNumber = number;
+            switch (info.test.Problems[number].Type.Id)
             {
                 case 0:
-                    return PartialView("_OneAnswerProblem",helpProblem[number]);
+                    return PartialView("_OneAnswerProblem",info);
                 case 1:
-                    return PartialView("_MultipleAnswerProblem", helpProblem[number]);
+                    return PartialView("_MultipleAnswerProblem", info);
                 case 2:
-                    return PartialView("_ToWriteTextProblem", helpProblem[number]);
+                    return PartialView("_ToWriteTextProblem", info);
                 case 3:
-                    return PartialView("_ToWriteCodeProblem", helpProblem[number]);
+                    return PartialView("_ToWriteCodeProblem", info);
                 case 4:
-                    return PartialView("_ToDrawDiagramProblem", helpProblem[number]);
+                    return PartialView("_ToDrawDiagramProblem", info);
             }
             return PartialView("_OneAnswerProblem");
         }
---
+++ LMS.Data/Models/TestInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LMS.Data.Models
+{
+    public class TestInfo
+    {
+        public Test test { get; set; }
+        public int CurrentQuestionNumber { get; set; }
+    }
+}
