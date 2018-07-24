f45615a213fcdf90b61634ca0a3c1223938af93d
Remza <megaremzik@gmail.com>
2018-07-24 11:33:38 +0300

    Add jQuery.serializeArray

--- LMS.Client.Web/wwwroot/js/Functions.js
+++ LMS.Client.Web/wwwroot/js/Functions.js
@@ -4,3 +4,11 @@
 function changeGot(got) {
     document.querySelector('input[name=got]').value = got;
 }
+function showValues() {
+    var fields = $(":input").serializeArray();
+    console.log($(":input").serializeArray());
+    $("#result").empty();
+    jQuery.each(fields, function (i, field) {
+        $("#result").append(field.value + " ");
+    });
+}
