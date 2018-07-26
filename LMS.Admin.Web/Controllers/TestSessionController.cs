285ba25a5d63eb9a29afa732f11d287a4ea09ab0
Maxim <Maxim1296@yandex.ru>
2018-07-26 11:38:49 +0300

    Test session calendar init

---
+++ LMS.Admin.Web/Controllers/TestSessionController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LMS.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS.Admin.Web.Controllers
+{
+    public class TestSessionController : Controller
+    {
+        public IActionResult List()
+        {
+            var session = new List<TestSessionDTO>
+            {
+                new TestSessionDTO()
+                {
+                    Id = 0,
+                    StartTime = DateTimeOffset.Now,
+                    Duration = TimeSpan.FromHours(2),
+                    Title = "Session #1"
+                },
+                new TestSessionDTO()
+                {
+                    Id = 1,
+                    StartTime = DateTimeOffset.Now - TimeSpan.FromDays(2),
+                    Duration = TimeSpan.FromHours(1.4),
+                    Title = "Session #2"
+                },
+                new TestSessionDTO()
+                {
+                    Id = 2,
+                    StartTime = DateTimeOffset.Now + TimeSpan.FromDays(2),
+                    Duration = TimeSpan.FromHours(2.3),
+                    Title = "Session #3"
+                }
+            };
+
+            return View(session);
+        }
+    }
+}
---
+++ LMS.Dto/TestSessionDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LMS.Dto
+{
+    public class TestSessionDTO
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public DateTimeOffset StartTime { get; set; }
+        public TimeSpan Duration { get; set; }
+
+        public DateTimeOffset EndTime => StartTime + Duration;
+
+        //public ICollection<Test> Variants { get; set; }
+    }
+}
