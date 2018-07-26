494923d8b371a89330ea3d50e16d259fd652f5e3
Olexseenko <olekseenko13@gmail.com>
2018-07-26 11:12:01 +0300

    Добавлен контролер сериализации для 1, N и открытого вопроса.

--- LMS.AnswerModels/SerializeControler.cs
+++ LMS.AnswerModels/SerializeControler.cs
@@ -1,4 +1,7 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
 
 namespace LMS.AnswerModels
 {
@@ -11,17 +14,33 @@ namespace LMS.AnswerModels
 
         public static object DeserializeJSON(string obj)
         {
-            var des = JsonConvert.DeserializeObject(obj);
-            switch (des)
+            try
             {
-                case SinglAnswer singl:
-                    return singl;
-                case MultyAnswer multy:
-                    return multy;
-                case OpenAnswer open:
-                    return open;
-                default:
-                    return null;
+                var des = JsonConvert.DeserializeObject<IDictionary<string, object>>(obj);
+                if (des.Count == 1)
+                {
+                    switch (des["Answer"])
+                    {
+                        case long singl:
+                            return new SinglAnswer(Convert.ToInt32(singl));
+                        case JArray multy:
+                            List<int> result = new List<int>();
+                            foreach (var item in multy.Children())
+                            {
+                                result.Add(int.Parse(item.ToString()));
+                            }
+                            return new MultyAnswer(result);
+                        case string open:
+                            return new OpenAnswer(open);
+                        default:
+                            return null;
+                    }
+                }
+                return null;
+            }
+            catch (System.Exception ex)
+            {
+                return null;
             }
         }
     }
--- LMS.Test/Services/SerealizationTest.cs
+++ LMS.Test/Services/SerealizationTest.cs
@@ -1,25 +1,40 @@
-﻿using System;
-using System.Linq;
-using LMS.Dto;
-using LMS.Interfaces;
-using LMS.Business.Services;
-using Moq;
-using Xunit;
-using Task = System.Threading.Tasks.Task;
+﻿using Xunit;
 using LMS.AnswerModels;
-
+using System.Collections.Generic;
 
 namespace LMS.Test.Services
 {
     public class SerealizationTest
     {
         [Fact]
-        public void serializeTest()
+        public void Serialize_Singl_Test()
         {
-            var str = "{\"answer\":2}";
-            SerializeControler.DeserializeJSON(str);
-            Assert.
-
+            var som = new SinglAnswer(2);
+            var ser = SerializeControler.SerializeToJSON(som);
+            Assert.Equal("{\"Answer\":2}", ser);
+            var answer = SerializeControler.DeserializeJSON(ser);
+            Assert.IsType<SinglAnswer>(answer);
+            Assert.Equal(som.ToString(), answer.ToString());
+        }
+        [Fact]
+        public void Serialize_Multy_Test()
+        {
+            var som = new MultyAnswer(new List<int> {1,2,3,4 });
+            var ser = SerializeControler.SerializeToJSON(som);
+            Assert.Equal("{\"Answer\":[1,2,3,4]}", ser);
+            var answer = SerializeControler.DeserializeJSON(ser);
+            Assert.IsType<MultyAnswer>(answer);
+            Assert.Equal(som.ToString(),answer.ToString());
+        }
+        [Fact]
+        public void Serialize_Open_Test()
+        {
+            var som = new OpenAnswer("Some text");
+            var ser = SerializeControler.SerializeToJSON(som);
+            Assert.Equal("{\"Answer\":\"Some text\"}", ser);
+            var answer = SerializeControler.DeserializeJSON(ser);
+            Assert.IsType<OpenAnswer>(answer);
+            Assert.Equal(som.ToString(), answer.ToString());
         }
     }
 }
