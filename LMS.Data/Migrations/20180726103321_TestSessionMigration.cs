a3cad49355de9ffd6c42d8d8f28d4c133a8bfdfc
Maxim <Maxim1296@yandex.ru>
2018-07-26 13:41:20 +0300

    Init test session migration

---
+++ LMS.Data/Migrations/20180726103321_TestSessionMigration.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LMS.Data.Migrations
+{
+    public partial class TestSessionMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Test_TestTemplates_TestTemplateId",
+                table: "Test");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TestLevel_Test_TestId",
+                table: "TestLevel");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Test",
+                table: "Test");
+
+            migrationBuilder.RenameTable(
+                name: "Test",
+                newName: "Tests");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Test_TestTemplateId",
+                table: "Tests",
+                newName: "IX_Tests_TestTemplateId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Tests",
+                table: "Tests",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Sessions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: true),
+                    StartTime = table.Column<DateTimeOffset>(nullable: false),
+                    Duration = table.Column<TimeSpan>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sessions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Examenee",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<string>(nullable: true),
+                    TestId = table.Column<int>(nullable: false),
+                    SessionId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Examenee", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Examenee_Sessions_SessionId",
+                        column: x => x.SessionId,
+                        principalTable: "Sessions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Examenee_Tests_TestId",
+                        column: x => x.TestId,
+                        principalTable: "Tests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Examenee_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TestSessionTest",
+                columns: table => new
+                {
+                    SessionId = table.Column<int>(nullable: false),
+                    TestId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestSessionTest", x => new { x.SessionId, x.TestId });
+                    table.ForeignKey(
+                        name: "FK_TestSessionTest_Sessions_SessionId",
+                        column: x => x.SessionId,
+                        principalTable: "Sessions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TestSessionTest_Tests_TestId",
+                        column: x => x.TestId,
+                        principalTable: "Tests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Examenee_SessionId",
+                table: "Examenee",
+                column: "SessionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Examenee_TestId",
+                table: "Examenee",
+                column: "TestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Examenee_UserId",
+                table: "Examenee",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestSessionTest_TestId",
+                table: "TestSessionTest",
+                column: "TestId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TestLevel_Tests_TestId",
+                table: "TestLevel",
+                column: "TestId",
+                principalTable: "Tests",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tests_TestTemplates_TestTemplateId",
+                table: "Tests",
+                column: "TestTemplateId",
+                principalTable: "TestTemplates",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TestLevel_Tests_TestId",
+                table: "TestLevel");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tests_TestTemplates_TestTemplateId",
+                table: "Tests");
+
+            migrationBuilder.DropTable(
+                name: "Examenee");
+
+            migrationBuilder.DropTable(
+                name: "TestSessionTest");
+
+            migrationBuilder.DropTable(
+                name: "Sessions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Tests",
+                table: "Tests");
+
+            migrationBuilder.RenameTable(
+                name: "Tests",
+                newName: "Test");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Tests_TestTemplateId",
+                table: "Test",
+                newName: "IX_Test_TestTemplateId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Test",
+                table: "Test",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Test_TestTemplates_TestTemplateId",
+                table: "Test",
+                column: "TestTemplateId",
+                principalTable: "TestTemplates",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TestLevel_Test_TestId",
+                table: "TestLevel",
+                column: "TestId",
+                principalTable: "Test",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
