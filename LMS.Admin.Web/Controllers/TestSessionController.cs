e5e64466287ab5075a7e7f9906b41ff526eb3706
Maxim <Maxim1296@yandex.ru>
2018-07-27 00:34:03 +0300

    TestSessionController + init views

--- LMS.Admin.Web/Controllers/TestSessionController.cs
+++ LMS.Admin.Web/Controllers/TestSessionController.cs
@@ -1,42 +1,116 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LMS.Business.Services;
 using LMS.Dto;
+using LMS.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
 
 namespace LMS.Admin.Web.Controllers
 {
+    [Authorize(Roles = "admin, moderator, reviewer")]
     public class TestSessionController : Controller
     {
-        public IActionResult List()
+        private readonly TestSessionService testSessionService;
+        private readonly TestTemplateService testTemplateService;
+        private readonly IdentityService identityService;
+
+        public TestSessionController(
+            TestSessionService testSessions,
+            TestTemplateService testTemplates,
+            IdentityService identity)
         {
-            var session = new List<TestSessionDTO>
+            testSessionService = testSessions;
+            testTemplateService = testTemplates;
+            identityService = identity;
+        }
+
+        [Authorize(Roles = "admin, moderator")]
+        public async Task<IActionResult> Create(DateTimeOffset? startTime = null)
+        {
+            var test = new TestSessionDTO()
             {
-                new TestSessionDTO()
-                {
-                    Id = 0,
-                    StartTime = DateTimeOffset.Now,
-                    Duration = TimeSpan.FromHours(2),
-                    Title = "Session #1"
-                },
-                new TestSessionDTO()
-                {
-                    Id = 1,
-                    StartTime = DateTimeOffset.Now - TimeSpan.FromDays(2),
-                    Duration = TimeSpan.FromHours(1.4),
-                    Title = "Session #2"
-                },
-                new TestSessionDTO()
-                {
-                    Id = 2,
-                    StartTime = DateTimeOffset.Now + TimeSpan.FromDays(2),
-                    Duration = TimeSpan.FromHours(2.3),
-                    Title = "Session #3"
-                }
+                StartTime = startTime ?? DateTimeOffset.Now
             };
 
-            return View(session);
+            ViewData["Templates"] = testTemplateService
+                .GetAll()
+                .Select(template => new SelectListItem
+                {
+                    Value = template.Id.ToString(),
+                    Text = template.Title
+                });
+            ViewData["Users"] = (await identityService
+                .GetAllAsync(Roles.Examinee))
+                .Select(user => new SelectListItem
+                {
+                    Value = user.Id,
+                    Text = user.Name
+                });
+
+            return View(test);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "admin, moderator")]
+        public async Task<IActionResult> Create([FromForm] TestSessionDTO testSession)
+        {
+            await testSessionService.CreateAsync(testSession);
+
+            return RedirectToAction(nameof(List));
+        }
+
+        [Authorize(Roles = "admin, moderator")]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var testSession = testSessionService.GetById(id);
+
+            ViewData["Templates"] = testTemplateService
+                .GetAll()
+                .Select(template => new SelectListItem
+                {
+                    Value = template.Id.ToString(),
+                    Text = template.Title
+                });
+            ViewData["Users"] = (await identityService
+                .GetAllAsync(Roles.Examinee))
+                .Select(user => new SelectListItem
+                {
+                    Value = user.Id,
+                    Text = user.Name
+                });
+
+            return View(testSession);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "admin, moderator")]
+        public async Task<IActionResult> Edit([FromForm] TestSessionDTO testSession)
+        {
+            await testSessionService.UpdateAsync(testSession);
+
+            return RedirectToAction(nameof(List));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "admin, moderator")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await testSessionService.DeleteByIdAsync(id);
+
+            return RedirectToAction(nameof(List));
+        }
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            var testSessions = testSessionService.GetAll();
+            return View(testSessions);
         }
     }
 }
