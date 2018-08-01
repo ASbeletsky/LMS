03129c119a3456bbef1f17da7b922c43f2e9c9d3
Remza <megaremzik@gmail.com>
2018-08-01 12:13:41 +0300

    Add AuthorizationController + LoginViewModel

---
+++ LMS.Client.Web/Controllers/AuthorizationController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LMS.Identity;
+using LMS.Client.Web.Models;
+namespace LMS.Client.Web.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        private IdentityService _identityService;
+
+        public AuthorizationController(IdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+
+        [HttpGet]
+        public IActionResult AccessDenied(Uri ReturnUrl)
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login(string returnUrl = null)
+        {
+            return View(new LoginViewModel { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _identityService.LogIn(model.UserName, model.Password, model.RememberMe);
+
+                    if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                        return Redirect(model.ReturnUrl);
+                    else
+                        return RedirectToAction("Greetings", "Home");
+                }
+                catch (Exception e)
+                {
+                    ModelState.AddModelError(string.Empty, e.Message);
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _identityService.Logout();
+            return RedirectToAction("Login", "Authorization");
+        }
+    }
+}
